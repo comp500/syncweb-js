53b7e188f155fe2a9edfdf03e10a54f79ca78a09
comp500 <comp500@users.noreply.github.com>
2018-01-07 19:29:28 +0000

    Satisfy eslint, and export Deserializer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+/* global SYNCPLAYEXPORT */
+
 import Client from "./Client";
 import Serializer from "./Serializer";
+import Deserializer from "./Deserializer";
 
 const SyncPlay = {};
 SyncPlay.Client = Client;
 SyncPlay.Serializer = Serializer;
+SyncPlay.Deserializer = Deserializer;
 
 export default SyncPlay;
 
