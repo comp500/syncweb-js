7f090743470101739dd86834f89bfc1450b84fdf
comp500 <comp500@users.noreply.github.com>
2018-01-19 14:48:04 +0000

    player and playerproxy getters/adders/removers

--- src/core/Client.js
+++ src/core/Client.js
@@ -2,11 +2,14 @@
 /* global ArrayHandlers */
 
 let staticProtocolList = [];
+let staticPlayerProxyList = [];
 
 class Client extends EventEmitter {
 	constructor() {
 		super();
 		this.protocolList = staticProtocolList;
+		this.playerList = [];
+		this.playerProxyList = staticPlayerProxyList;
 	}
 
 	connect() {
@@ -37,6 +40,41 @@ class Client extends EventEmitter {
 		ArrayHandlers.remove(staticProtocolList, protocol);
 	}
 
+	addPlayer(player) {
+		this.playerList.push(player);
+	}
+
+	getPlayer(player) {
+		return ArrayHandlers.get(this.playerList, player);
+	}
+
+	removePlayer(player) {
+		ArrayHandlers.remove(this.playerList, player);
+	}
+
+	addPlayerProxy(playerProxy) {
+		this.playerProxyList.push(playerProxy);
+	}
+
+	getPlayerProxy(playerProxy) {
+		return ArrayHandlers.get(this.playerProxyList, playerProxy);
+	}
+
+	removePlayerProxy(playerProxy) {
+		ArrayHandlers.remove(this.playerProxyList, playerProxy);
+	}
+
+	static addStaticPlayerProxy(playerProxy) {
+		staticPlayerProxyList.push(playerProxy);
+	}
+
+	static getStaticPlayerProxy(playerProxy) {
+		return ArrayHandlers.get(staticPlayerProxyList, playerProxy);
+	}
+
+	static removeStaticPlayerProxy(playerProxy) {
+		ArrayHandlers.remove(staticPlayerProxyList, playerProxy);
+	}
 	
 }
 
