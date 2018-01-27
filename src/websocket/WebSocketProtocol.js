95b53a77df1a7ba505a0daa567dd975260cc26af
comp500 <comp500@users.noreply.github.com>
2018-01-27 18:49:06 +0000

    Add loads of code for protocol

--- src/websocket/WebSocketProtocol.js
+++ src/websocket/WebSocketProtocol.js
@@ -12,6 +12,38 @@ class WebSocketProtocol extends SyncWeb.Protocol {
 
 		this.socket.addEventListener("message", (e) => {
 			this.emit("message", e.data);
+
+			let parsed = JSON.parse(e.data);
+			console.log("SERVER:", parsed); // eslint-disable-line no-console
+
+			if (parsed.Error) {
+				console.log("err", parsed.Error); // eslint-disable-line no-console
+			}
+
+			if (parsed.Hello) {
+				console.log("hello", parsed.Hello); // eslint-disable-line no-console
+			}
+
+			if (parsed.Set) {
+				console.log("set", parsed.Set); // eslint-disable-line no-console
+			}
+
+			if (parsed.List) {
+				console.log("list", parsed.List); // eslint-disable-line no-console
+			}
+
+			if (parsed.State) {
+				console.log("state", parsed.State); // eslint-disable-line no-console
+				if (parsed.State.ping.yourLatency != null) {
+					this.clientRtt = parsed.State.ping.yourLatency;
+				}
+				this.latencyCalculation = parsed.State.ping.latencyCalculation;
+				if (parsed.State.ignoringOnTheFly && parsed.State.ignoringOnTheFly.server) {
+					this.serverIgnoringOnTheFly = parsed.State.ignoringOnTheFly.server;
+					this.clientIgnoringOnTheFly = 0;
+					this.stateChanged = false;
+				}
+			}
 		});
 	}
 
@@ -19,6 +51,43 @@ class WebSocketProtocol extends SyncWeb.Protocol {
 		if (command == "send") {
 			this.socket.send(data);
 		}
+	}
+
+	sendState() {
+		let clientIgnoreIsNotSet = (this.clientIgnoringOnTheFly == 0 || this.serverIgnoringOnTheFly != 0);
+		let output = {};
+		output.State = {};
+
+		if (clientIgnoreIsNotSet) {
+			output.State.playstate = {};
+			output.State.playstate.position = 0.0;
+			output.State.playstate.paused = true;
+			// if seek, send doSeek: true and then set seek to false
+		}
+
+		output.State.ping = {};
+		output.State.ping.latencyCalculation = this.latencyCalculation;
+		output.State.ping.clientLatencyCalculation = Date.now() / 1000;
+		output.State.ping.clientRtt = this.clientRtt;
+
+		if (this.stateChanged) {
+			this.clientIgnoringOnTheFly += 1;
+		}
+
+		if (this.serverIgnoringOnTheFly > 0 || this.clientIgnoringOnTheFly > 0) {
+			output.State.ignoringOnTheFly = {};
+			if (this.serverIgnoringOnTheFly > 0) {
+				output.State.ignoringOnTheFly.server = this.serverIgnoringOnTheFly;
+				this.serverIgnoringOnTheFly = 0;
+			}
+			if (this.clientIgnoringOnTheFly > 0) {
+				output.State.ignoringOnTheFly.client = this.clientIgnoringOnTheFly;
+			}
+		}
+
+		console.log(output); // eslint-disable-line no-console
+
+		return output;
 	}
 }
 
