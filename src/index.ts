84fb246e1cdfe72f6f14a38c7825907a13a274bd
comp500 <comp500@users.noreply.github.com>
2019-07-24 23:09:18 +0100

    Expose EventTracker

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 // TODO: TCP client?
 export { default as SyncplayJSONClient } from "./protocols/SyncplayJSONClient";
 export { default as JSONMessageProtocol } from "./protocols/JSONMessageProtocol";
+// TODO: move more code out of syncweb-frontend
+export { default as EventTracker } from "./util/EventTracker";
 export * from "./protocols/SyncplayJSONClient";
--- src/syncweb.ts
+++ src/syncweb.ts
@@ -1,4 +1,5 @@
 // Used to produce a web-compatible UMD file
 export { default as SyncplayJSONClient } from "./protocols/SyncplayJSONClient";
 export { default as JSONMessageProtocol } from "./protocols/JSONMessageProtocol";
+export { default as EventTracker } from "./util/EventTracker";
 export * from "./protocols/SyncplayJSONClient";
