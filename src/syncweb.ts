1defe1ea1d01805d5c1b1360bab6289827e4e44c
comp500 <comp500@users.noreply.github.com>
2019-07-24 22:41:11 +0100

    Improve layout slightly hopefully TS will import index.ts directly

--- src/syncweb.ts
+++ src/syncweb.ts
@@ -1,3 +1,4 @@
+// Used to produce a web-compatible UMD file
 export { default as SyncplayJSONClient } from "./protocols/SyncplayJSONClient";
 export { default as JSONMessageProtocol } from "./protocols/JSONMessageProtocol";
 export * from "./protocols/SyncplayJSONClient";
